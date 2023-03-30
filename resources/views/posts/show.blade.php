cef2e4f300b7ea5cf362151b0f7649fbc6b5b084
ruchi306 <ruchi306@icloud.com>
2023-03-30 18:22:32 +0900

    カリキュラム8－3復習

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,25 +1,20 @@
-<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        
     </head>
-    <body class="antialiased">
-        <h1 class='title'>
-            {{ $post->title}}
-        </h1>    
-        <div class='content'>
-              <div class='content_post'>
-                  <h3>本文</h3>
-                   <p class='body'>{{ $post->body }}</p>
-             </div>
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                   <a href="/posts/{{ $post->id}}"> <h2 class='title'>{{ $post->title }}</h2></a>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
         </div>
-        <div class='footer'>
-            <a href="/">戻る</a>
-        </div>
+        <div class='Paginate'>{{ $posts->links()}}</div>
     </body>
 </html>
